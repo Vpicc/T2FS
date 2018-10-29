91962a3ba4052dfc55810d503c7db7902ea3d3c2
Victor Piccoli <victorpicc@gmail.com>
2018-10-29 13:04:26 -0300

    Teste da apidisk

--- t2fs/teste/testApiDisk.c
+++ t2fs/teste/testApiDisk.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 #include "../include/apidisk.h"
 
+// -lm -m32
+// gcc -o teste testApiDisk.c ../lib/apidisk.o -Wall -ggdb && ./teste
 int main() {
+    int i;
     unsigned char buffer[256] = {0};
-    buffer[1] = 1;
-    printf("\nHello\n");
     read_sector(0,buffer);
-    printf("%s\n", buffer);
-    getchar();
+    for(i = 0; i < sizeof(buffer)/sizeof(unsigned char); i++){
+        if ( i == 0 ) {
+            printf("\nID:\n");
+        }
+        if ( i < 4 ) {
+            printf("%c ", buffer[i]);
+        }
+        if ( i == 4 ) {
+            printf("\nversion:\n");
+        }
+        if ( i == 6 ) {
+            printf("\nSuperBlockSize\n");
+        }
+        if ( i == 8 ) {
+            printf("\nDiskSize\n");
+        }
+        if ( i == 12 ) {
+            printf("\nNofSectors\n");
+        }
+        if ( i == 16 ) {
+            printf("\nSectorsPerCluster\n");
+        }
+        if ( i == 20 ) {
+            printf("\npFATSectorStart\n");
+        }
+        if ( i == 24 ) {
+            printf("\nRootDirCluster\n");
+        }
+        if ( i == 28 ) {
+            printf("\nDataSectorStart\n");
+        }
+        if ( i >= 4 && i < 32) {
+            printf("%x ", buffer[i]);
+        }
+    }
+    printf("\n");
     return 0;
 }
